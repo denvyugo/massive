d79297e0b37f5cd59d43dcc2653b381dbcd3e4d2
denvyugo <denvyugo@gmail.com>
2018-09-09 22:26:50 +0300

    task 01 for homework 06

--- task02.py
+++ task02.py
@@ -10,7 +10,9 @@ for i, item in enumerate(list):
     if item % 2 == 0:
         idx.append(i)
 
-print(list, f'размер массива {sys.getsizeof(list)}')
-print(idx, f'размер массива {sys.getsizeof(idx)}')
+print(list, f'размер массива {sys.getsizeof(list)}') # 192 байта
+print(idx, f'размер массива {sys.getsizeof(idx)}') # 128 байт
 print(f'размеры всех переменных '
       f'{sys.getsizeof(list) + sys.getsizeof(idx) + sys.getsizeof(limit) + sys.getsizeof(size) + sys.getsizeof(i)}')
+# всего 404 байта
+# Python 3.7, Windows 10, 64bit
