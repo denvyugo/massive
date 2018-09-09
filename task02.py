955a70d8a14714f3c822a9691890a9cb4d4c5d79
denvyugo <denvyugo@gmail.com>
2018-09-09 21:57:19 +0300

    task 01 for homework 06

--- task02.py
+++ task02.py
@@ -1,5 +1,6 @@
 # Во втором массиве сохранить индексы четных элементов первого массива
 import random
+import sys
 
 limit = 10
 size = 10
@@ -9,5 +10,7 @@ for i, item in enumerate(list):
     if item % 2 == 0:
         idx.append(i)
 
-print(list)
-print(idx)
+print(list, f'размер массива {sys.getsizeof(list)}')
+print(idx, f'размер массива {sys.getsizeof(idx)}')
+print(f'размеры всех переменных '
+      f'{sys.getsizeof(list) + sys.getsizeof(idx) + sys.getsizeof(limit) + sys.getsizeof(size) + sys.getsizeof(i)}')
